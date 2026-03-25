e9d11966d14dd19975b0904fc679a2688344c43b
NarendraGore <ngore4981@gmail.com>
2026-03-25 12:04:47 +0530

    Book Controller added with Post and get Method

---
+++ EFCoreLearning/Controllers/BookController.cs
@@ -0,0 +1,46 @@
+﻿using EFCoreLearning.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreLearning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public BookController(AppDbContext appDbContext)
+        {
+
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllBook()
+        {
+            var result = await _appDbContext.Books.ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpPost("")]
+
+        public async Task<IActionResult> AddBookAsync([FromBody] Book model)
+        {
+            model.CreatedOn = DateTime.UtcNow;
+            _appDbContext.Books.Add(model);
+            await _appDbContext.SaveChangesAsync();
+            return Ok(model);
+        }
+
+        [HttpPost("bulk")]
+
+        public async Task<IActionResult> AddBooks([FromBody] List<Book> model)
+        {
+            _appDbContext.Books.AddRange(model);
+            await _appDbContext.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}

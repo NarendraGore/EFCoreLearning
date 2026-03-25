569e32bb9cd83e21a84ad8322bdb76cd038e0617
NarendraGore <ngore4981@gmail.com>
2026-03-25 15:07:27 +0530

    Author model

---
+++ EFCoreLearning/Models/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreLearning.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}

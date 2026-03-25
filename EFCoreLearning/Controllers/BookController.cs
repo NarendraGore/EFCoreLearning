7a2b61dea1fc12cb704888feadae416077413e61
NarendraGore <ngore4981@gmail.com>
2026-03-25 15:06:59 +0530

    Update Method added for books

--- EFCoreLearning/Controllers/BookController.cs
+++ EFCoreLearning/Controllers/BookController.cs
@@ -34,13 +34,40 @@ namespace EFCoreLearning.Controllers
             return Ok(model);
         }
 
-        [HttpPost("bulk")]
+        [HttpPost("single")]
 
-        public async Task<IActionResult> AddBooks([FromBody] List<Book> model)
+        public async Task<IActionResult> AddBooks([FromBody] Book model)
         {
-            _appDbContext.Books.AddRange(model);
+
+            //var author = new Author()
+            //{
+            //    Name="Test1",
+            //    Email = "test1@gmail.com"
+
+            //};
+            //model.Author= author;
+            _appDbContext.Books.Add(model);
             await _appDbContext.SaveChangesAsync();
             return Ok();
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBookById([FromRoute] int id, [FromBody] Book model) {
+            var book = _appDbContext.Books.FirstOrDefault(x => x.Id == id);
+            if (book == null) {
+                return NotFound();
+
+            }
+
+            book.Title = model.Title;
+            book.AuthorId = model.AuthorId;
+            book.NoOfPages = model.NoOfPages;
+            book.Description = model.Description;
+
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok(book);
         }
     }
 }
